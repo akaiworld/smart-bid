6c875f91f06e6e18af32d2c10da349806deac278
Anton <akai-world@narod.ru>
2019-12-11 11:30:25 +0200

    init

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// import '@/fns/index';
+// import App from '@/app';
+
+// ReactDOM.render(<App />, document.getElementById("app"));
